7f3321c4004c7eb159bf0b27ccf04b70939532d1
Ismayil Ismayilov <Ismayil.Ismayilov@unibank.az>
2026-07-20 11:29:00 +0400

    feat: rendering accounts page and display accounts

--- src/app/pages/accounts/accounts.ts
+++ src/app/pages/accounts/accounts.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Account } from '../../core/models/account.model';
+import { AccountService } from '../../core/services/account.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css',
 })
-export class Accounts {}
+export class Accounts {
+  private accountService = inject(AccountService);
+
+  accounts = signal<Account[]>([]);
+  isLoading = signal(true);
+  hasError = signal(false);
+
+  constructor() {
+    this.loadAccountsData();
+  }
+
+  private loadAccountsData() {
+    this.isLoading.set(true);
+    this.hasError.set(false);
+
+    this.accountService.getAccounts().subscribe({
+      next: (accounts) => {
+        this.accounts.set(accounts);
+        this.isLoading.set(false);
+      },
+      error: () => {
+        this.hasError.set(true);
+        this.isLoading.set(false);
+      },
+    });
+  }
+
+}
