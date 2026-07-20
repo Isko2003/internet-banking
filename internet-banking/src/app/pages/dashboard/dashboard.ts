ebe0b3112cb55bfb596d8bd7115e83109fb2f58c
Ismayil Ismayilov <Ismayil.Ismayilov@unibank.az>
2026-07-20 10:03:59 +0400

    feat: adding expense charts to dashboard page by using chart.js

--- internet-banking/src/app/pages/dashboard/dashboard.ts
+++ internet-banking/src/app/pages/dashboard/dashboard.ts
@@ -16,11 +16,12 @@ import { RouterLink } from '@angular/router';
 import { NotificationService } from '../../core/services/notification.service';
 import { AppNotification } from '../../core/models/notification.model';
 import { NotificationRow } from "../../shared/components/notification-row/notification-row";
+import { ExpenseChart } from '../../shared/components/expense-chart/expense-chart';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, CurrencyPipe, DashboardPanel, AccountRow, CardRow, TransactionRow, RouterLink, NotificationRow],
+  imports: [CommonModule, CurrencyPipe, DashboardPanel, AccountRow, CardRow, TransactionRow, RouterLink, NotificationRow, ExpenseChart],
   templateUrl: './dashboard.html',
 })
 export class Dashboard {
@@ -36,6 +37,7 @@ export class Dashboard {
   accounts = signal<Account[]>([]);
   cards = signal<Card[]>([]);
   transactions = signal<Transaction[]>([]);
+  allTransactions = signal<Transaction[]>([]);
   notifications = signal<AppNotification[]>([]);
 
   isLoading = signal(true);
@@ -55,6 +57,7 @@ export class Dashboard {
     accounts: this.accountService.getAccounts(),
     cards: this.cardService.getCards(),
     transactions: this.transactionService.getRecent(5),
+    allTransactions: this.transactionService.getTransactions(),
     notifications: this.notificationService.getNotifications(),
   })
     .pipe(
@@ -64,15 +67,17 @@ export class Dashboard {
           accounts: [] as Account[],
           cards: [] as Card[],
           transactions: [] as Transaction[],
+          allTransactions: [] as Transaction[],
           notifications: [] as AppNotification[],
         });
       }),
       finalize(() => this.isLoading.set(false))
     )
-    .subscribe(({ accounts, cards, transactions, notifications }) => {
+    .subscribe(({ accounts, cards, transactions, notifications, allTransactions }) => {
       this.accounts.set(accounts);
       this.cards.set(cards);
       this.transactions.set(transactions);
+      this.allTransactions.set(allTransactions);
       this.notifications.set(notifications);
       this.totalBalance.set(accounts.reduce((sum: number, a: Account) => sum + a.balance, 0));
     });
